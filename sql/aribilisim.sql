26d4df1b4ccbd9733d6fb32949fa409aa317cbce
suhedayuksel <102050667+suhedayuksel@users.noreply.github.com>
2022-03-21 19:52:31 +0300

    sql güncellendi

---
+++ sql/aribilisim.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1:3306
+-- Üretim Zamanı: 21 Mar 2022, 16:51:51
+-- Sunucu sürümü: 5.7.31
+-- PHP Sürümü: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `aribilisim`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kategoriler`
+--
+
+DROP TABLE IF EXISTS `kategoriler`;
+CREATE TABLE IF NOT EXISTS `kategoriler` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kategori` varchar(25) NOT NULL,
+  `katturu` varchar(15) NOT NULL,
+  `ustkat` varchar(30) NOT NULL,
+  `meta` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `kategoriler`
+--
+
+INSERT INTO `kategoriler` (`id`, `kategori`, `katturu`, `ustkat`, `meta`) VALUES
+(6, 'html', 'Alt Kategori', 'web tasarım', 'html, bir web sitesinin temel kod yapısıdır.'),
+(3, 'web tasarım', 'Ana Kategori', '-', 'web tasarım ve kodlama üzerine bilgiler'),
+(4, 'grafik tasarım', 'Ana Kategori', '-', 'grafik tasarım ve prensipleri hakkında bilgiler'),
+(5, 'dijital pazarlama', 'Ana Kategori', '-', 'sosyal medya ve google süreçleri hakkında bilgiler');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `ornek`
+--
+
+DROP TABLE IF EXISTS `ornek`;
+CREATE TABLE IF NOT EXISTS `ornek` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ad` varchar(20) NOT NULL,
+  `yas` int(3) NOT NULL,
+  `il` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `ornek`
+--
+
+INSERT INTO `ornek` (`id`, `ad`, `yas`, `il`) VALUES
+(6, 'Kaan', 40, 'İstanbul'),
+(8, 'Hayko', 40, 'İstanbul');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
